cf20acbb59cde4a1a62f58ddc9e3514a3850b104
Iegor Maiorov <maiorov.prog@gmail.com>
2017-06-10 00:23:48 +0300

    few experiments with embeding jersey

---
+++ embedded-jetty-container-sample/src/main/java/app/Demo.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		EmbeddedJettyServer embeddedJettyServer = new EmbeddedJettyServer();
+		embeddedJettyServer.start();
+	}
+
+}
---
+++ embedded-jetty-container-sample/src/main/java/app/EmbeddedJettyServer.java
@@ -0,0 +1,19 @@
+package app;
+
+import org.eclipse.jetty.server.Server;
+
+public class EmbeddedJettyServer {
+
+	private Server server;
+	private int port = 8089;
+
+	public EmbeddedJettyServer() {
+//		server = new Server(port);
+//		server.start();
+	}
+
+	public void start() {
+
+	}
+
+}
