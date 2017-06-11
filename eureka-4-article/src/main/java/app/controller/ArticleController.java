7b1d7ebfdb32ba90a06103dda34e692fe40eee6a
Iegor Maiorov <maiorov.prog@gmail.com>
2017-06-11 13:48:40 +0300

    implemented few microservices

--- eureka-4-article/src/main/java/app/controller/ArticleController.java
+++ eureka-4-article/src/main/java/app/controller/ArticleController.java
@@ -1,27 +1,18 @@
-package app.controllers;
+package app.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DefaultController {
-
-	private Logger log = LoggerFactory.getLogger(DefaultController.class);
+public class ArticleController {
 
 	@Value("${words}")
 	private String words;
 
-	public DefaultController() {
-		log.info("Words are: " + words);
-	}
-
 	@RequestMapping("/")
-	public String getWord() {
+	public String getArticle() {
 		String[] wordArray = words.split(",");
-		log.info("Words are: " + words);
 		int i = (int)Math.round(Math.random() * (wordArray.length - 1));
 		return wordArray[i];
 	}
